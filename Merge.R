ebd84fc5d699e763e103feb38e50e5a812497a00
LuXinxue <109177389+LuXinxue@users.noreply.github.com>
2022-12-08 15:44:14 -0500

    Summary

---
+++ Merge.R
@@ -0,0 +1,6 @@
+library(caret)
+library(dplyr)
+
+SNAP <- read.csv("C:/Users/96209/Documents/GitHub/Final-Project-Group-3/SNAP Benefits_County Level_2013 edit.csv")
+Fin <- read.csv("C:/Users/96209/Documents/GitHub/Final-Project-Group-3/Finances2013.csv")
+
